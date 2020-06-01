2738dad4ec10c84986badd1a155534be98c0d45d
Rafael Lopez <rafael.iskoat@gmail.com>
2020-05-31 19:32:19 -0500

    Initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from "react"
+import { Link, useLocation } from "wouter"
+import getGifs from '../../services/getGifs'
+import ListOfGifs from '../../components/ListOfGifs'
+import Category from '../../components/Category'
+import {useGifs} from '../../hooks/useGifs'
+
+const POPULAR_GIFS = ["Matrix", "Venezuela", "Chile", "Colombia", "Ecuador"]
+
+export default function Home() {
+  const [keyword, setKeyword] = useState('')
+  const [path, pushLocation] = useLocation()
+  const {loading, gifs} = useGifs()
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    // navegar a otra ruta
+    pushLocation(`/search/${keyword}`)
+  }
+
+  const handleChange = evt => {
+    setKeyword(evt.target.value)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
+        <button>Buscar</button>
+      </form>
+      <h3 className="App-title">Última búsqueda</h3>
+      <ListOfGifs gifs={gifs} />
+      <Category name="Categorias populares" options={POPULAR_GIFS}/>
+    </>
+  )
+}
